c554d580d3ac1a64111950b005da653bc715f646
Mikael Theovaldo <theovaldomikael@gmail.com>
2026-06-04 00:11:03 -0300

    Classe ParticipanteRepository finalizada com todos seus metodos implementados

--- src/main/java/com/secretKeeper/repository/ParticipanteRepository.java
+++ src/main/java/com/secretKeeper/repository/ParticipanteRepository.java
@@ -1,4 +1,29 @@
-package secretKeeper.repository;
+package main.java.com.secretKeeper.repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParticipanteRepository {
+    private static final List<String> participantes = new ArrayList<>();
+
+    public static void adicionar(String nome){
+        participantes.add(nome);
+    }
+
+    public static void remover(int indice){
+        participantes.remove(indice);
+    }
+
+    public static List<String> listar(){
+        return participantes;
+    }
+
+    public static int totalParticipante(){
+        return participantes.size();
+    }
+
+    public boolean existe(String nome){
+        return participantes.stream()
+                .anyMatch(p -> p.equalsIgnoreCase(nome));
+    }
 }
