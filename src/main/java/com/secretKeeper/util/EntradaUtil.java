82f29ab193262a57c1eec5ea4fb112729772d984
Mikael Theovaldo <theovaldomikael@gmail.com>
2026-06-10 16:33:21 -0300

    util finalizado

--- src/main/java/com/secretKeeper/util/EntradaUtil.java
+++ src/main/java/com/secretKeeper/util/EntradaUtil.java
@@ -1,4 +1,33 @@
-package secretKeeper.util;
+package main.java.com.secretKeeper.util;
+
+import java.util.Scanner;
 
 public class EntradaUtil {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    // Lê números inteiros
+    public static int lerInteiro(String mensagem) {
+
+        while (true) {
+            try {
+                System.out.print(mensagem);
+
+                int valor = Integer.parseInt(scanner.nextLine().trim());
+
+                return valor;
+
+            } catch (NumberFormatException e) {
+                System.out.println("Digite um número inteiro válido.");
+            }
+        }
+    }
+
+    // Lê texto
+    public static String lerTexto(String mensagem) {
+
+        System.out.print(mensagem);
+
+        return scanner.nextLine().trim();
+    }
 }
