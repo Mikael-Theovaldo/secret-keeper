ce257b888f36bb4bb431b90840b39838888b47c2
Mikael Theovaldo <theovaldomikael@gmail.com>
2026-06-10 15:44:47 -0300

    classe Main pronta para compilar sem erros

--- src/main/java/com/secretKeeper/Main.java
+++ src/main/java/com/secretKeeper/Main.java
@@ -1,13 +1,10 @@
-package com.secretKeeper.aplication;
+package com.secretKeeper;
 
 import com.secretKeeper.views.Menu;
-import com.secretKeeper.service.FormacaoTimesService;
-import com.secretKeeper.service.OrdemApresentacaoService;
-import com.secretKeeper.service.SorteioSimplesService;
-import com.secretKeeper.model.Participante;
 
 public class Main {
     public static void main(String[] args) {
         new Menu().iniciar();
+
     }
 }
