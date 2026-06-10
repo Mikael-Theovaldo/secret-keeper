bd06706cc86a32356d5a92907345ad4f18f02f18
rocksDjoao <joaopaulo2006borges@gmail.com>
2026-06-09 22:57:12 -0300

    classe OrdemApresentaçãoService v1

--- src/main/java/com/secretKeeper/service/OrdemApresentacaoService.java
+++ src/main/java/com/secretKeeper/service/OrdemApresentacaoService.java
@@ -1,4 +1,18 @@
 package main.java.com.secretKeeper.service;
 
+
+
 public class OrdemApresentacaoService {
+    public List<String> gerarOrdem(List<String> participantes) {
+
+        if (participantes == null || participantes.isEmpty()) {
+            throw new IllegalArgumentException("Nenhum participante cadastrado.");
+        }
+
+        List<String> ordem = new ArrayList<>(participantes);
+
+        Collections.shuffle(ordem, new Random());
+
+        return ordem;
+    }
 }
